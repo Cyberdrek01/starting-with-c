820c94be02818c09e36ff6e47c5c1edca44994aa
cyberdrek01 <144948926+Cyberdrek01@users.noreply.github.com>
2024-04-19 09:37:11 +0530

    chapter2

--- areaofsquare.c
+++ areaofsquare.c
@@ -1,9 +1,37 @@
-#include<stdio.h>
+// #include<stdio.h>
+// #include<math.h>
 
-int main(){
+// int main(){
+//     int side;
+//     printf("Enter side:");
+//     scanf("%d", side);
+//     int power = pow(side,2);
+//     printf("area of square is: %d", power);
+//     return 0;
+// }
+#include <stdio.h>
+#include <math.h>
+
+// Function to calculate the area of a square
+int calculateSquareArea(int side) {
+    int area = pow(side, 2);
+    return area;
+}
+
+int main() {
     int side;
-    printf("Enter side:");
-    scanf("%d",&side);
-    printf("area of square is: %d", side*side);
+
+    // Prompt user to enter the side length of the square
+    printf("Enter the side length of the square: ");
+
+    // Read the side length from user input
+    scanf("%d", &side);
+
+    // Calculate the area of the square
+    int area = calculateSquareArea(side);
+
+    // Print the area of the square
+    printf("The area of the square is: %d\n", area);
+
     return 0;
 }
