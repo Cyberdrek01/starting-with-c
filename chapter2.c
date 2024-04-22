1dc50215e1bbb15341b53a9a8bb144cdd9b04acb
cyberdrek01 <144948926+Cyberdrek01@users.noreply.github.com>
2024-04-22 23:24:29 +0530

    prctice5

--- chapter2.c
+++ chapter2.c
@@ -1,16 +1,23 @@
 // type declaration instruction declare var  before using it
 
-#include <stdio.h>
-#include <math.h>
+// #include <stdio.h>
+// #include <math.h>
 
-int main() {
-    int a=15;
-    int b= a;
-    int c= b+1;
-    printf("c=%d\n",c);
-    return 0;
-}
+// int main() {
+//     int a=15;
+//     int b= a;
+//     int c= b+1;
+//     printf("c=%d\n",c);
+//     return 0;
+// }
 // arithmetic instruction + - *  / %  to define power use pow e.g: a=pow(b,2)
 // comparison instruction == != > >= < <= 
 // logic instruction && || !
 // bitwise AND | ^ ~
+
+
+// operator precedence
+// associativity left to right
+
+// 1=true
+// 0=false
