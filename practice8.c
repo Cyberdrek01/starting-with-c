9d12a5a8ba7f0c629437c49a44a4093c28d90191
cyberdrek01 <144948926+Cyberdrek01@users.noreply.github.com>
2024-04-24 21:59:19 +0530

    Q2

--- practice8.c
+++ practice8.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 int main(){
-    int isSunday=1;
-    int isSnowing=1;
-    printf("%d\n", isSunday && isSnowing);
+    int isMonday=1;
+    int isRaining=1;
+    printf("%d\n", isMonday || isRaining);
     return 0;
 }
