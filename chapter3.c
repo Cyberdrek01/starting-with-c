02813478f083655867d72b5c98cb31ba1e63c339
cyberdrek01 <144948926+Cyberdrek01@users.noreply.github.com>
2024-04-24 22:53:12 +0530

    switch code

--- chapter3.c
+++ chapter3.c
@@ -1,20 +1,48 @@
-//conditional statement
-#include <stdio.h>
+// //conditional statement
+// #include <stdio.h>
+
+// int main(){
+//     int age;
+//     printf("enter age: ");
+//     scanf("%d",&age);
+    
+//     age>=18 ? printf("adult\n") : printf("not Adult\n");
+//     return 0;
+// }
+// ternary
+// Condition?doSomething if True: doSomething if False;
+
+// switch
+// switch(number){
+//     case C1://*code*/
+//         break;
+//     case C2:/*code*/
+//         break;
+//     default://*default action*
+// }
+
+#include<stdio.h>
 
 int main(){
-    int age;
-    printf("enter age: ");
-    scanf("%d",&age);
-    if (age>18)
-    {
-        printf("adult\n");
-    }
-    else if (age>13&&age<18)
-    {
-        printf("teenager\n");
-    }
-    else{
-        printf("Baccha");
+    int day;
+    printf("enter day(1-7):");
+    scanf("%d",&day);
+    switch(day){
+        case 1:printf("Monday\n");
+            break;
+        case 2:printf("Tuesday\n");
+            break;
+        case 3:printf("Wednesday\n");
+            break;
+        case 4:printf("Thursday\n");
+            break;
+        case 5:printf("Friday\n");
+            break;
+        case 6:printf("Saturday\n");
+            break;
+        case 7:printf("Sunday\n");
+            break;
+        default: printf("not a valid day!\n");
     }
     return 0;
 }
