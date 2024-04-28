5819c61411131bced56050f0f403e6e6fe3ac0fd
cyberdrek01 <144948926+Cyberdrek01@users.noreply.github.com>
2024-04-28 22:24:42 +0530

    00

--- chapter3.c
+++ chapter3.c
@@ -21,28 +21,44 @@
 //     default://*default action*
 // }
 
-#include<stdio.h>
+// #include<stdio.h>
+
+// int main(){
+//     int day;
+//     printf("enter day(1-7):");
+//     scanf("%d",&day);
+//     switch(day){
+//         case 1:printf("Monday\n");
+//             break;
+//         case 2:printf("Tuesday\n");
+//             break;
+//         case 3:printf("Wednesday\n");
+//             break;
+//         case 4:printf("Thursday\n");
+//             break;
+//         case 5:printf("Friday\n");
+//             break;
+//         case 6:printf("Saturday\n");
+//             break;
+//         case 7:printf("Sunday\n");
+//             break;
+//         default: printf("not a valid day!\n");
+//     }
+//     return 0;
+// }
+
+#include <stdio.h>
 
 int main(){
-    int day;
-    printf("enter day(1-7):");
-    scanf("%d",&day);
-    switch(day){
-        case 1:printf("Monday\n");
-            break;
-        case 2:printf("Tuesday\n");
-            break;
-        case 3:printf("Wednesday\n");
-            break;
-        case 4:printf("Thursday\n");
-            break;
-        case 5:printf("Friday\n");
-            break;
-        case 6:printf("Saturday\n");
-            break;
-        case 7:printf("Sunday\n");
-            break;
-        default: printf("not a valid day!\n");
+    int number;
+    printf("enter number:");
+    scanf("%d", &number);
+
+    if (number>=0);
+    {
+        printf("positive \n");
+    }else{
+        
     }
-    return 0;
+    
 }
