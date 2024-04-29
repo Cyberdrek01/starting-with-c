aa47e504b9c4f1932f2c3955a4f0e23f5e2c56b4
cyberdrek01 <144948926+Cyberdrek01@users.noreply.github.com>
2024-04-29 08:47:02 +0530

    practice9

---
+++ practice9.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+    int marks;
+    printf("enter the marks(0-100): ");
+    scanf("%d", marks);
+
+    if  (marks >30){
+        printf("pass \n");
+}else{
+    printf("fail");
+}
+return 0;
+}
