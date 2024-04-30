a20a2d7d958d9aa0a2a824be0e5d8ada7f0ce160
cyberdrek01 <144948926+Cyberdrek01@users.noreply.github.com>
2024-04-30 09:08:03 +0530

    practice15

--- chapter4.c
+++ chapter4.c
@@ -4,9 +4,21 @@
 #include <stdio.h>
 
 int main() {
-    for(int i=1; i<=5; i=i+1){
-        printf("hello world \n");
-    }
+    // for(int i=1; i<=5; i=i+1){
+    //     printf("hello world \n");
+    // }
 
+
+    // int i=1;
+    // while(i<=5){
+    //     printf("%d \n",i);
+    //     i++;
+    // }
+
+    int i=1;
+    do{
+        printf("%d \n" ,i);
+        i++;
+    }while(i<=5);
     return 0;
 }
