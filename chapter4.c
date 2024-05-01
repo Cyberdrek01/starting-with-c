100ed40af2a55e704cf52a3666a4477737c55f8c
cyberdrek01 <144948926+Cyberdrek01@users.noreply.github.com>
2024-05-01 22:43:07 +0530

    practice17

--- chapter4.c
+++ chapter4.c
@@ -1,24 +1,37 @@
 // loop control instruction
 // for while do while
 
+// #include <stdio.h>
+
+// int main() {
+//     // for(int i=1; i<=5; i=i+1){
+//     //     printf("hello world \n");
+//     // }
+
+
+//     // int i=1;
+//     // while(i<=5){
+//     //     printf("%d \n",i);
+//     //     i++;
+//     // }
+
+//     int i=1;
+//     do{
+//         printf("%d \n" ,i);
+//         i++;
+//     }while(i<=5);
+//     return 0;
+// }
+
+// break statement
 #include <stdio.h>
-
-int main() {
-    // for(int i=1; i<=5; i=i+1){
-    //     printf("hello world \n");
-    // }
-
-
-    // int i=1;
-    // while(i<=5){
-    //     printf("%d \n",i);
-    //     i++;
-    // }
-
-    int i=1;
-    do{
-        printf("%d \n" ,i);
-        i++;
-    }while(i<=5);
+int main(){
+    for(int i=1; i<=5; i++){
+        if(i==3){
+            break;
+        }
+        printf("%d \n", i);
+    }
+    printf("end");
     return 0;
 }
