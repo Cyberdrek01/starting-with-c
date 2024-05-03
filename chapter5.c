e666870ffee13a6d96bd4eea893e1e1f547fb6a8
cyberdrek01 <144948926+Cyberdrek01@users.noreply.github.com>
2024-05-03 08:48:34 +0530

    practice21

--- chapter5.c
+++ chapter5.c
@@ -2,3 +2,20 @@
 
 // fuction : block of code that performs particular task
 // recursion: calling a function from within itself
+
+#include <stdio.h>
+void printHello();
+void printGoodbye();
+
+int main(){
+    printHello();
+    printGoodbye();
+    return 0;
+}
+
+void printHello(){
+    printf("Hello \n");
+}
+void printGoodbye(){
+    printf("Goodbye\n");
+}
