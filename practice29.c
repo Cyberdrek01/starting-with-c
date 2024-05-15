f6d669dac2a1d1b53527a3a5b6a768f89ad35f3a
cyberdrek01 <144948926+Cyberdrek01@users.noreply.github.com>
2024-05-15 22:28:37 +0530

    practice29

---
+++ practice29.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <math.h>
+int main(){
+    int n;
+    printf("Enter the number: ");
+    scanf("%d",&n);
+    // s=pow(n,2);
+    printf("The square of the number is: %f",pow(n,2));
+
+    return 0;
+}
