15343db87642198cdfa1fe36a34ae9f6cce8181b
cyberdrek01 <144948926+Cyberdrek01@users.noreply.github.com>
2024-05-16 22:38:37 +0530

    2

--- cvc.c
+++ cvc.c
@@ -1,9 +1,23 @@
-#include<stdio.h>
+// #include<stdio.h>
+// int main(){
+//     float a,b;
+//     printf("Enter two numbers: ");
+//     scanf("%f %f",&a,&b);
+//     float Q=a/b;
+//     printf("The quotient will be: %f",Q);
+//     return 0;
+// }
+
+#include <stdio.h>
 int main(){
     float a,b;
     printf("Enter two numbers: ");
     scanf("%f %f",&a,&b);
-    float Q=a/b;
-    printf("The quotient will be: %f",Q);
+    if (a>b)
+    {
+        printf("a is max");
+    }
+    else{printf("b is max");
+    }
     return 0;
 }
