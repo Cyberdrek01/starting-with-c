e31bb05accb146221aaf6ccbdc0cb0db16b5e679
cyberdrek01 <144948926+Cyberdrek01@users.noreply.github.com>
2024-05-24 00:31:55 +0530

    cmmit

--- cvc.c
+++ cvc.c
@@ -8,16 +8,33 @@
 //     return 0;
 // }
 
+// #include <stdio.h>
+// int main(){
+//     float a,b;
+//     printf("Enter two numbers: ");
+//     scanf("%f %f",&a,&b);
+//     if (a>b)
+//     {
+//         printf("a is max");
+//     }
+//     else{printf("b is max");
+//     }
+//     return 0;
+// }
+
 #include <stdio.h>
 int main(){
-    float a,b;
-    printf("Enter two numbers: ");
-    scanf("%f %f",&a,&b);
-    if (a>b)
+    int N;
+    printf("Enter a number: ");
+    scanf("%d",&N);
+    int I=1;
+    int Sum=0;
+    while (I<=N)
     {
-        printf("a is max");
-    }
-    else{printf("b is max");
+        Sum=Sum+I;
+        printf(" The sum is: %d \n",Sum);
+        I++;
     }
     return 0;
 }
+    
--- practice12.c
+++ practice12.c
@@ -1,14 +1,13 @@
-#include<stdio.h>
-
-int main(){
-    char ch;
-    printf("Enter character: ");
-    scanf("%c",&ch);
-
-    if (ch>='A' && ch<='Z'){
-        printf("character is uppercase");
-    }else{
-        printf("character is not uppercase");
-    }
-    return 0;
-}
+#include <stdio.h>  
+int main()  
+{  
+    int i, n, sum=0;  
+    printf("Enter any number: ");  
+    scanf("%d", &n);  
+    for(i=2; i<=n; i+=2)  
+    {  
+        sum += i;  
+    }  
+    printf("Sum of all even numbers from 1 to %d: %d", n, sum);  
+    return 0;  
+}  
