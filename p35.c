d9b4ad16511dc4b37ce5d7c8f84908af675590c7
cyberdrek01 <144948926+Cyberdrek01@users.noreply.github.com>
2024-05-31 23:01:57 +0530

    p35

--- p35.c
+++ p35.c
@@ -2,5 +2,19 @@
 int main(){
     int ram,shyam,ajay;
     printf("Enter the age of Ram: ");
-    scanf("%d",)
+    scanf("%d",&ram);
+    printf("Enter the age of shyam:");
+    scanf("%d",&shyam);
+    printf("Enter the age of Ajay: ");
+    scanf("%d",&ajay);
+    if(ram>=shyam && ram>ajay){
+        printf("Ram is the oldest person");
+    }
+    else if(shyam>ram  && shyam>ajay){
+        printf("Shyam is oldest");
+    }
+    else{
+        printf("Ajay is oldest");
+    }
+    return 0;
 }
