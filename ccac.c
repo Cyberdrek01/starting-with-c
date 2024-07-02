ec4a2e01e74153f5b3831d00dc44911a8b1266dd
cyberdrek01 <144948926+Cyberdrek01@users.noreply.github.com>
2024-07-02 23:53:06 +0530

    next no.

---
+++ ccac.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int i=3;
+    switch(i){
+        case 0:
+        printf("Customers are dicey\n");
+        case 1+0:
+        printf("Markets are pricey");
+        case 4/2:
+        printf("Investors are moody");
+    }
+    return 0;
+}
