3ae688e7185f1a6eba96145a7c949bbac3c4dc6d
cyberdrek01 <144948926+Cyberdrek01@users.noreply.github.com>
2024-07-07 22:56:07 +0530

    gregorian

---
+++ greg.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+int main()
+{
+    //basic year is chosen as the least year which user can enter through the keyboard where it should be Monday on 01th of January
+    int year, basic_year=1900, leap_year, remaining_year, total_days, day;
+
+    printf("Enter the year: ");
+    scanf("%d", &year);
+
+    year = (year-1)-basic_year; 
+    leap_year = year/4;
+
+ 
+    remaining_year = year - leap_year;
+
+
+    total_days = (remaining_year*365) + (leap_year*366) + 1;
+
+    day = total_days%7;
+
+    if(day==0)
+        printf("Monday");
+    else if(day==1)
+        printf("Tuesday");
+    else if(day==2)
+        printf("Wednesday");
+    else if(day==3)
+        printf("Thursday");
+    else if(day==4)
+        printf("Friday");
+    else if(day==5)
+        printf("Saturday");
+    else if(day==6)
+        printf("Sunday");
+    else
+        printf("Wrong Entry");
+    return 0;
+}
